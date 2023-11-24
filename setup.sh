d48fe1e8e5d577950ce8d856e5173c367348cb30
daige <3556158925@qq.com>
2023-11-24 10:50:32 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
+sudo apt update
 sudo apt-get install git-lfs
 git lfs pull
 git submodule init
 git submodule update
-sudo apt update && sudo apt install -y build-essential libbenchmark-dev libomp-dev libgmp-dev nlohmann-json3-dev postgresql libpqxx-dev libpqxx-doc nasm libsecp256k1-dev grpc-proto libsodium-dev libprotobuf-dev libssl-dev cmake libgrpc++-dev protobuf-compiler protobuf-compiler-grpc uuid-dev
+sudo apt install -y build-essential libbenchmark-dev libomp-dev libgmp-dev nlohmann-json3-dev postgresql libpqxx-dev libpqxx-doc nasm libsecp256k1-dev grpc-proto libsodium-dev libprotobuf-dev libssl-dev cmake libgrpc++-dev protobuf-compiler protobuf-compiler-grpc uuid-dev
