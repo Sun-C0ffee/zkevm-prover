644fab34e43f0e49e99c3aa1a62262b7bd5c31ec
daige <3556158925@qq.com>
2023-11-24 10:00:02 +0800

    add run script and config.json

---
+++ run.sh
@@ -0,0 +1,2 @@
+make -j
+./build/zkProver -c ./config.json
---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install git-lfs
+git lfs pull
+git submodule init
+git submodule update
+sudo apt update && sudo apt install -y build-essential libbenchmark-dev libomp-dev libgmp-dev nlohmann-json3-dev postgresql libpqxx-dev libpqxx-doc nasm libsecp256k1-dev grpc-proto libsodium-dev libprotobuf-dev libssl-dev cmake libgrpc++-dev protobuf-compiler protobuf-compiler-grpc uuid-dev
