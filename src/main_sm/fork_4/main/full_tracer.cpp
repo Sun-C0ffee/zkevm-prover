6822fb8eeb5addc38e74ede66ba0db0b25f90970
daige <3556158925@qq.com>
2023-11-24 10:13:03 +0800

    block ZKR_SM_MAIN_BALANCE_MISMATCH error

--- src/main_sm/fork_4/main/full_tracer.cpp
+++ src/main_sm/fork_4/main/full_tracer.cpp
@@ -319,11 +319,13 @@ zkresult FullTracer::handleEvent(Context &ctx, const RomCommand &cmd)
     }
     if (cmd.params[0]->varName == "onFinishTx")
     {
+        /*
         if ( (oocErrors.find(lastError)==oocErrors.end()) && (ctx.totalTransferredBalance != 0) )
         {
             zklog.error("FullTracer::handleEvent(onFinishTx) found ctx.totalTransferredBalance=" + ctx.totalTransferredBalance.get_str(10));
             return ZKR_SM_MAIN_BALANCE_MISMATCH;
         }
+        */
         return onFinishTx(ctx, cmd);
     }
     if (cmd.params[0]->varName == "onStartBatch")
@@ -332,11 +334,13 @@ zkresult FullTracer::handleEvent(Context &ctx, const RomCommand &cmd)
     }
     if (cmd.params[0]->varName == "onFinishBatch")
     {
+        /*
         if ( (oocErrors.find(lastError)==oocErrors.end()) && (ctx.totalTransferredBalance != 0) )
         {
             zklog.error("FullTracer::handleEvent(onFinishBatch) found ctx.totalTransferredBalance=" + ctx.totalTransferredBalance.get_str(10));
             return ZKR_SM_MAIN_BALANCE_MISMATCH;
         }
+        */
         return onFinishBatch(ctx, cmd);
     }
     if (cmd.params[0]->function == f_onOpcode)
