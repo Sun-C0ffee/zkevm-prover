82e3074b2813bf4c7caa202f365b30e8bd09a0d2
daige <3556158925@qq.com>
2023-12-05 15:28:37 +0800

    update config

--- gen_proof.sh
+++ gen_proof.sh
@@ -1,6 +1,7 @@
 make -j
 # ./build/zkProver -c ./poc_config/batchProof/config_runFile_BatchProof.json
 ./build/zkProver -c ./poc_config/aggregatedProof/config_runFile_AggregatedProof.json
+#./build/zkProver -c ./poc_config/finalProof/config_runFile_FinalProof.json
 
 
 
